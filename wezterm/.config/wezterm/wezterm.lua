750d458671d2fc122b1c01f6a2d493b3a8113e48
Dejan <dex.antic@gmail.com>
2026-07-02 00:50:59 +0200

    style(wezterm): run stylua on config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,14 +1,14 @@
 -- Pull in WezTerm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- Initialize actual config
 local config = {}
 if wezterm.config_builder then
-    config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 -- Appearance
-config.font = wezterm.font 'JetBrainsMono Nerd Font'
+config.font = wezterm.font("JetBrainsMono Nerd Font")
 config.font_size = 14.0
 config.color_scheme = "Tokyo Night"
 config.window_decorations = "RESIZE"
@@ -17,13 +17,14 @@ config.native_macos_fullscreen_mode = false
 
 -- Keybindings
 config.keys = {
-    -- Default QuickSelect keybind (CTRL - SHIFT - Space) gets captrued by something
-    -- else on my system
-    {
-        key = 'A',
-        mods = 'CTRL|SHIFT',
-        action = wezterm.action.QuickSelect,
-    }
+	-- Default QuickSelect keybind (CTRL - SHIFT - Space) gets captrued by something
+	-- else on my system
+	{
+		key = "A",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.QuickSelect,
+	},
 }
 
 return config
+
